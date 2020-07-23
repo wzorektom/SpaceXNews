886607b4f057a4d2c027a4b05b5e36167da0a4be
Thomsd <wzorektom@googlemail.com>
2020-07-23 09:39:44 +0100

    Startup URI corrected and navigation menu fixed

--- Controls/NavPanel.xaml.cs
+++ Controls/NavPanel.xaml.cs
@@ -25,12 +25,14 @@ namespace SpacexNews.Controls
 
         private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
         {
-
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
         }
 
         private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
         {
-
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
         }
     }
 }
